8d0f992d8d10b66ec547932c72ac7ae86a294caa
falahsuta <13518102@std.stei.itb.ac.id>
2020-09-03 16:16:44 +0700

    Updates

---
+++ precache-manifest.ac1ed2bf51dbff08d22095f7c6fcae89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d7293ed3c7540ed699a5218c4ed8ba",
+    "url": "/dev/index.html"
+  },
+  {
+    "revision": "0df5038edccf2dd1718c",
+    "url": "/dev/static/css/main.bc001087.chunk.css"
+  },
+  {
+    "revision": "e09c9b535f2f27675386",
+    "url": "/dev/static/js/2.8c69801b.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/dev/static/js/2.8c69801b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df5038edccf2dd1718c",
+    "url": "/dev/static/js/main.2469d493.chunk.js"
+  },
+  {
+    "revision": "0ecbb9f18e3e8b783494",
+    "url": "/dev/static/js/runtime-main.dc913cb8.js"
+  }
+]);
