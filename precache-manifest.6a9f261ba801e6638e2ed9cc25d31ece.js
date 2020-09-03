9528f97716e1bedb181c67b00bc1bbc863886c7c
falahsuta <13518102@std.stei.itb.ac.id>
2020-09-03 13:35:14 +0700

    Updates

---
+++ precache-manifest.6a9f261ba801e6638e2ed9cc25d31ece.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9050c9906edf37b22078d1ec8eface",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0df5038edccf2dd1718c",
+    "url": "/static/css/main.bc001087.chunk.css"
+  },
+  {
+    "revision": "e09c9b535f2f27675386",
+    "url": "/static/js/2.8c69801b.chunk.js"
+  },
+  {
+    "revision": "8dea9a823bfb24207fef47cb32cf30ad",
+    "url": "/static/js/2.8c69801b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df5038edccf2dd1718c",
+    "url": "/static/js/main.2469d493.chunk.js"
+  },
+  {
+    "revision": "cf839f7d6a8ccd21a5db",
+    "url": "/static/js/runtime-main.c70acead.js"
+  }
+]);
