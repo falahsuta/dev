487619bc16dad9d4e7476b27f7a110cb824174e4
moh.sutawindaya <moh.sutawindaya@gopay.co.id>
2026-01-20 13:37:15 +0700

    feat: support mobile (revert point mobile changes: 5.0)
    -> desc: exp and project finaling

--- src/components/pages/projects/data-detail-proj.js
+++ src/components/pages/projects/data-detail-proj.js
@@ -6,6 +6,7 @@ export const detailData = {
     features:
       "The UI features including dark mode, dialog based interactions (creating post, submitting form, viewing post), threaded comments style, infinite fetch posts (server-side pagination), tags categories, and also multi-step validation form. In server-side it also has authentication and authorization system (jwt-token).",
     height: 74,
+    mobileHeight: 74,
     gif: "https://media4.giphy.com/media/j3mZ0w4038EKNMtZuI/giphy.gif",
     linkto: "https://github.com/falahsuta/Projects-1-Unamed",
     giflink: "https://media2.giphy.com/media/iFsJzdqaQ3v66NGXXZ/giphy.gif",
@@ -18,6 +19,7 @@ export const detailData = {
       "Input is very open to any news/articles related, search-engine styles app that gives all information relevant. It uses variance algorithm to extract the information such as boyer moore, KMP, and simple regex",
     gif: "https://media3.giphy.com/media/J4hThbQO89hS5HHp2m/giphy.gif",
     height: 70,
+    mobileHeight: 70,
     linkto: "https://github.com/falahsuta/Info-Extraction",
     giflink: "https://media2.giphy.com/media/hqg68DMCEhLhvekK3d/giphy.gif",
   },
@@ -27,6 +29,7 @@ export const detailData = {
     features:
       "Single page application, page transition, some dialog interactions, and simple animations",
     height: 63,
+    mobileHeight: 63,
     gif: "https://media3.giphy.com/media/RG4AAhncolcEuHNiox/giphy.gif",
     linkto: "https://github.com/falahsuta/Personal-Web-",
     giflink: "https://media3.giphy.com/media/UWzoTLkoxDW8t1EASN/giphy.gif",
@@ -38,7 +41,8 @@ export const detailData = {
     features:
       "Detail information related to particular problems, and shows step-by-step the solving processes.",
     gif: "https://media1.giphy.com/media/LkwIrR6xRrs0jO98Yb/giphy.gif",
-    height: 67,
+    height: 68,
+    mobileHeight: 68,
     linkto: "https://github.com/falahsuta/15-solver",
     giflink: "https://media3.giphy.com/media/eg9wtzO4w6nhdsjXs4/giphy.gif",
   },
