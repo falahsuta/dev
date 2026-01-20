777a79541c093f1211eee0b8d030580d794c1460
moh.sutawindaya <moh.sutawindaya@gopay.co.id>
2026-01-20 15:02:15 +0700

    feat: update about me

--- src/components/pages/about/dynamic-content.js
+++ src/components/pages/about/dynamic-content.js
@@ -2,7 +2,7 @@ export default {
   "About Me": {
     title: "About Me",
     desc:
-      "Hello there! I’m Falah Suta—feel free to call me Falah or Suta. This is my personal space where I share my background and interests. I studied Computer Science at Institut Teknologi Bandung (class of 2018–2022). I’ve always been fascinated by understanding how things work, which naturally led me to computer science. Building things in the digital space unlocks endless possibilities, and that’s what excites me most about this field. In my free time, you’ll usually find me playing games, tinkering with computers, or binge-watching shows—I’m pretty much chronically online.",
+      "Hello there! I’m Falah Suta—feel free to call me Falah or Suta. This is my personal space where I share my background and interests. I studied Computer Science at Institut Teknologi Bandung (class of 2018–2022). I always have my fascination about dissecting how things work, which naturally led me to computer science, there’s lots of cool things we can do by making things in digital space! During free time you’d find me either playing games, tinkering computer stuff, or straight binge watching show— I’m like chronically online atp. By the way, ",
     contact: "Lets collaborate & connect! ",
   },
 };
