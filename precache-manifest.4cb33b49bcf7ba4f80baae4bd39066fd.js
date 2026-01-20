297f6bf2c5b3deec478b31ea08b4f84f497fd56e
moh.sutawindaya <moh.sutawindaya@gopay.co.id>
2026-01-20 13:30:33 +0700

    Updates

--- precache-manifest.4cb33b49bcf7ba4f80baae4bd39066fd.js
+++ precache-manifest.4cb33b49bcf7ba4f80baae4bd39066fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c18d403c6af55ae1fddd0b262e5718e",
+    "revision": "9269a31e7607a9c80cd71451306c9a35",
     "url": "/dev/index.html"
   },
   {
-    "revision": "18a57bb33fbfabbaf092",
+    "revision": "1dedd3e51899fb6f0b78",
     "url": "/dev/static/css/main.61f32ee0.chunk.css"
   },
   {
-    "revision": "40493c372bfc6428325a",
-    "url": "/dev/static/js/2.0caac270.chunk.js"
+    "revision": "32c30f429af0ed1614e7",
+    "url": "/dev/static/js/2.83838546.chunk.js"
   },
   {
     "revision": "8dea9a823bfb24207fef47cb32cf30ad",
-    "url": "/dev/static/js/2.0caac270.chunk.js.LICENSE.txt"
+    "url": "/dev/static/js/2.83838546.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "18a57bb33fbfabbaf092",
-    "url": "/dev/static/js/main.7d63729c.chunk.js"
+    "revision": "1dedd3e51899fb6f0b78",
+    "url": "/dev/static/js/main.a6494927.chunk.js"
   },
   {
     "revision": "0ecbb9f18e3e8b783494",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev/precache-manifest.4d2123480de23e803a91f7061ce30926.js"
+  "/dev/precache-manifest.4cb33b49bcf7ba4f80baae4bd39066fd.js"
 );
 
 self.addEventListener('message', (event) => {
